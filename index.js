cc258e5eeb8c6e662d550003d54b1ea753e6383b
Yummyzinn <enricozagallo92@gmail.com>
2024-02-13 22:54:06 -0300

    relogio-final

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
-const relogio = setInterval(function time(){ 
+const relogio = setInterval(function time() { 
 let data = new Date();
-let hora = data.getHours();
-let min = data.getMinutes();
-let sec = data.getSeconds();
+let hora =  data.getHours();
+let min =  data.getMinutes();
+var sec = data.getSeconds();
 
 
 document.getElementById('hor').textContent = hora
-document.getElementById('min').innerHTML = min
+document.getElementById('min').textContent = min
 document.getElementById('sec').textContent = sec
+
+
+if( sec < 10 ) {
+    document.getElementById('sec').innerHTML = '0'+ sec
+}
+else{
+    
+}
+
+
+
+
+if( min < 10 ) {
+    document.getElementById('min').innerHTML = '0'+ min
+}
+else{
+    
+}
+
+if( hora < 10 ) {
+    document.getElementById('hor').innerHTML = '0'+ hora
+}
+else{
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
